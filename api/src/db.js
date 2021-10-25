44ef8cdeefd216475f6db392057b534652bc5c8a
LOLE81 <martinezpm@gmail.com>
2021-10-25 11:44:06 -0300

    first commit

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const { Recipe, Diet } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Recipe.belongsToMany(Diet, {through: 'recipediet'});
+Diet.belongsToMany(Recipe, {through: 'recipediet'});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define('diet', {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
@@ -14,18 +14,5 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    summary: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    score: {
-      type: DataTypes.INTEGER
-    },
-    healthScore: {
-      type: DataTypes.INTEGER 
-    },
-    steps: {
-      type: DataTypes.STRING
-    }
   });
 };
